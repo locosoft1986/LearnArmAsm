508b5c1bd61ea32d69b98ded6b626efdfe60b11d
Jeremy Weasley <locosoft1986@gmail.com>
2016-03-15 23:32:46 +0800

    Complete part2 start up to clear bss section

---
+++ part2/main.c
@@ -0,0 +1,24 @@
+#include "gpio.h"
+
+//GPIO Register set
+volatile unsigned int* gpio;
+
+//loop variable
+volatile unsigned int tim;
+
+void cmain(void)
+{
+  gpio = (unsigned int*) GPIO_BASE;
+
+  gpio[LED_GPFSEL] |= (1 << LED_GPFBIT);
+
+  while(1) {
+    for (tim = 0; tim < 500000; ++tim);
+
+    gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
+
+    for (tim = 0; tim < 500000; ++tim);
+
+    gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
+  }
+}
